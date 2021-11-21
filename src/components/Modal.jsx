9a2a19e7ffe981665db8bb61b1139dffb293ef22
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-11-21 14:46:50 -0600

    [update]: close modal to click around the alert

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createPortal } from 'react-dom';
 import { MdClose } from 'react-icons/md';
+import AppContext from 'contexts/AppContext';
 import 'styles/Modal.scss';
 
 const Modal = ({ children, isActive, changeStatus }) => {
+    const { state } = useContext(AppContext);
+
+    const themeClass = state.darkTheme ? ' Dark' : '';
+
+    const closeModalWithBackground = (event) => {
+        const currentClass = event.target.classList.value;
+        if (currentClass === 'Modal Dark') {
+            changeStatus();
+        }
+    };
+
     if (isActive) {
         return createPortal(
-            <div className="Modal">
+            <div className={`Modal${themeClass}`} onClick={closeModalWithBackground}>
                 <div className="Modal__Content">
                     <MdClose className="Modal__Content--Close" onClick={changeStatus} />
                     {children}
